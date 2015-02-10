04939dc6e616a2f7744d9b6c68f6830bc287c75d
Sebastian Helzle <sebastian@helzle.net>
2015-02-10 20:43:16 +0100

    [TASK] Replace sendmail with mailhog container
    * Webinterface which runs on port 8025
    * Much easier setup than sendmail.

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -3,8 +3,5 @@
 # Exit with error if a command returns a non-zero status
 set -e
 
-# Start service sendmail
-service sendmail start
-
 # Run php-fpm as last process that keeps running, otherwise docker container will exit
 php5-fpm
